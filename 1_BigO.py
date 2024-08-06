68ae4862836177a45ba413815d72358ce7d7d823
B-Rohan-Rao <raorohan060@gmail.com>
2024-08-06 10:14:56 +0530

    fixes

--- 1_BigO.py
+++ 1_BigO.py
@@ -7,3 +7,16 @@ def print_number(n):
 
 
 print_number(10)
+
+"""
+For above, the loops are running independently so it will be O(n)
+For next example, we can see nested loop resulting in O(n^2)
+"""
+
+def print_number(n):
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+
+
+print_number = 10
--- 1_O(n)^2.py
+++
@@ -1,7 +0,0 @@
-def print_number(n):
-    for i in range(n):
-        for j in range(n):
-            print(i, j)
-
-
-print_number = 10
